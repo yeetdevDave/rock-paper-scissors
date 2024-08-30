dec38226a398ead11a95faf22ce453759ee1fefe
Dave <107414336+yeetdevDave@users.noreply.github.com>
2024-08-30 23:23:02 +0200

    playRound() working

---
+++ script.js
@@ -0,0 +1,44 @@
+let humanScore = 0
+let computerScore = 0
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getComputerChoice() {
+  let arr = ["rock", "paper", "scissors"]
+
+  let index = randomIntFromInterval(0, 2)
+
+  return arr[index]
+}
+
+function getHumanChoice() {
+  let choice = prompt("Choose between rock, paper, scissors:")
+
+  return choice
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase()
+  computerChoice = computerChoice.toLowerCase()
+
+  if((humanChoice == "rock" && computerChoice == "paper") || 
+    (humanhoice == "paper" && computerChoice == "scissors") || 
+    (humanChoice == "scissors" && computerChoice == "rock")) {
+      computerScore++
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+  }else if((computerChoice == "rock" && humanChooice == "paper") || 
+  (computerChoice == "paper" && humanChoice == "scissors") || 
+  (computerChoice == "scissors" && humanChoice == "rock")) {
+    humanScore++
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+  }
+  else if(humanChoice == computerChoice)
+    console.log("Tie!")
+}
+
+let humanChoice = getHumanChoice()
+let computerChoice = getComputerChoice()
+
+playRound(humanChoice, computerChoice)
